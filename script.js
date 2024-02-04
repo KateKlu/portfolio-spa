77cc6d19aedc7ddc41db4c9a4f02931f2f8386f4
Katerina Kliuieva <kate.kliuieva@gmail.com>
2024-02-04 07:37:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,99 @@
+/////////////      Slider      ///////////////
+let slideIndex = 0;
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const slider = document.querySelector('.slider');
+const slides = document.querySelectorAll('.slide');
+
+function showSlide() {
+    const slideWidth = slides[0].clientWidth;
+    slider.style.transform = `translateX(${-slideIndex * slideWidth}px)`;
+}   
+
+function nextSlide() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide();
+}
+
+prevBtn.addEventListener('click', () => {
+    slideIndex = (slideIndex - 1 + slides.length) % slides.length;
+    showSlide();
+})
+
+nextBtn.addEventListener('click', () => {nextSlide()})
+
+// Automatic slide change 
+setInterval(() => {nextSlide();}, 10000);
+
+
+///////////////        BurgerMenu          ////////////
+function clickBurgerMunu () {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    burgerMenu.addEventListener('click', () => {
+        navLinks.classList.toggle('show')});
+ 
+    navLinks.addEventListener('click', () => {
+        navLinks.classList.remove('show')});
+}
+
+clickBurgerMunu();    
+
+    
+//////////////////     Login / Logout      /////////////////
+const loginForm = document.querySelector('#loginForm');
+const logoutBtn = document.querySelector('#logout');
+
+loginForm.addEventListener('submit', (event) => {
+    const adminName = 'kk';
+    const adminPassword = '111';
+    const usernameInput = document.querySelector('#username').value;
+    const passwordInput = document.querySelector('#password').value;
+
+    if (usernameInput === adminName && passwordInput === adminPassword) {
+        showHideSections('admin');
+        loginForm.reset();
+    } else {
+        event.preventDefault;
+        alert('Invalid username or password. Please try again.');
+    }
+})
+
+logoutBtn.addEventListener('click', () => {showHideSections('home')})
+
+
+//////////////   Site navigation    ///////////
+const navLinks = document.querySelectorAll('.nav-link');
+const sections = document.querySelectorAll('section');
+const projectBtn = document.querySelector('#projectBtn');
+const contactsBtn = document.querySelector('#contactsBtn');
+
+function findCurrent() {
+    for (let section of sections) {
+        if (!section.classList.contains('hidden')) {
+            return section;            
+        }
+    }
+}
+
+function showHideSections(linkId) {
+    const currentSection = findCurrent(); 
+
+    if (!currentSection.classList.contains(linkId)){
+        currentSection.classList.add('hidden');
+
+        for (let section of sections) {
+            if (section.classList.contains(linkId)) {
+                section.classList.remove('hidden');
+                break;
+            }
+        }
+    }        
+}
+
+navLinks.forEach((link) => link.addEventListener('click', () => {showHideSections(link.id)}));
+
+projectBtn.addEventListener('click', () => {showHideSections('projects')})
+
+contactsBtn.addEventListener('click', () => {showHideSections('contacts')})
